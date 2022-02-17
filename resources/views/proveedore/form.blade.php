868666bf7a0855ae1299b23ae9c03c7fc8ef84f7
DavidMenesess <david.menesess@udea.edu.co>
2022-02-17 07:07:56 -0500

    Ruta login

--- resources/views/proveedore/form.blade.php
+++ resources/views/proveedore/form.blade.php
@@ -1,49 +1,52 @@
 <div class="box box-info padding-1">
-    <div class="box-body">
+    <div class="box-body text-center">
         
-        <div class="form-group">
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Nit_Empresa') }}
             {{ Form::text('Nit_Empresa', $proveedore->Nit_Empresa, ['class' => 'form-control' . ($errors->has('Nit_Empresa') ? ' is-invalid' : ''), 'placeholder' => 'Nit Empresa']) }}
             {!! $errors->first('Nit_Empresa', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </div><br>
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Nombre') }}
             {{ Form::text('Nombre', $proveedore->Nombre, ['class' => 'form-control' . ($errors->has('Nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
             {!! $errors->first('Nombre', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </div><br>
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Apellido') }}
             {{ Form::text('Apellido', $proveedore->Apellido, ['class' => 'form-control' . ($errors->has('Apellido') ? ' is-invalid' : ''), 'placeholder' => 'Apellido']) }}
             {!! $errors->first('Apellido', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </div><br>
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Empresa') }}
             {{ Form::text('Empresa', $proveedore->Empresa, ['class' => 'form-control' . ($errors->has('Empresa') ? ' is-invalid' : ''), 'placeholder' => 'Empresa']) }}
             {!! $errors->first('Empresa', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </div><br>
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Categoria') }}
             {{ Form::text('Categoria', $proveedore->Categoria, ['class' => 'form-control' . ($errors->has('Categoria') ? ' is-invalid' : ''), 'placeholder' => 'Categoria']) }}
             {!! $errors->first('Categoria', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </div><br>
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Dirreccion') }}
             {{ Form::text('Dirreccion', $proveedore->Dirreccion, ['class' => 'form-control' . ($errors->has('Dirreccion') ? ' is-invalid' : ''), 'placeholder' => 'Dirreccion']) }}
             {!! $errors->first('Dirreccion', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </div><br>
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Telefono') }}
             {{ Form::text('Telefono', $proveedore->Telefono, ['class' => 'form-control' . ($errors->has('Telefono') ? ' is-invalid' : ''), 'placeholder' => 'Telefono']) }}
             {!! $errors->first('Telefono', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </div><br>
+        <div class="form-group col-md-4 offset-md-4">
             {{ Form::label('Correo') }}
             {{ Form::text('Correo', $proveedore->Correo, ['class' => 'form-control' . ($errors->has('Correo') ? ' is-invalid' : ''), 'placeholder' => 'Correo']) }}
             {!! $errors->first('Correo', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
+        </div><br>
 
     </div>
-    <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="box-footer mt20 offset-md-4">
+    <br><button type="submit" class="btn btn-primary">Guardar</button>
+        <a type="" href="{{ route('proveedores.index') }}" class="btn btn-danger bg-opacity-100
+         offset-1 ">Cancelar</a>
+
     </div>
 </div>
